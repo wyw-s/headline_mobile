c3ce59e88fa70b0885145016bcfc0c4fa92187a5
wyw-s <wyw.s@foxmail.com>
2019-12-11 18:42:41 +0800

    处理已登录和未登录的显示状态

--- src/views/tabbar/index.vue
+++ src/views/tabbar/index.vue
@@ -6,7 +6,14 @@
       <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
       <van-tabbar-item icon="search">问答</van-tabbar-item>
       <van-tabbar-item icon="friends-o">视频</van-tabbar-item>
-      <van-tabbar-item icon="setting-o" to="/my">我的</van-tabbar-item>
+      <!--
+      路由在切换时。旧组件会被销毁，但是vuex里面的数据不会被销毁；
+      所以把用户的登录信息储存到vuex中，可以用做登录时的判断
+      -->
+      <van-tabbar-item
+          icon="setting-o"
+          to="/my"
+      >{{ $store.state.Token ? '我的' : '未登录' }}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
