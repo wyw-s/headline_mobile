48b1bbc2bc774909dba1b4d7da4a45681881d431
wyw-s <wyw.s@foxmail.com>
2019-12-14 17:54:13 +0800

    解决移动端点击 300ms 延迟

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
 // 加载Vant组件js文件；
 import './utils/register-vant.js'
 // 加载amfe-flexible 模块，以设置基准rem的值
@@ -11,6 +12,7 @@ import './styles/index.less'
 import './utils/TimeHandling.js'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
